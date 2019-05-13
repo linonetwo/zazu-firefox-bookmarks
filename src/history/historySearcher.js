775c5279a1136447d58fa9b3dd8e123326813864
linonetwo <linonetwo012@gmail.com>
2019-05-13 18:28:12 +0800

    refactor: use sqlite in pure js

--- src/history/historySearcher.js
+++ src/history/historySearcher.js
@@ -1,33 +1,38 @@
 // @ts-check
-const sqlite = require('sqlite');
+const initSqlJs = require('sql.js');
 const path = require('path');
+const promisify = require('util.promisify');
+const fs = require('fs');
 
-const tryDecodeURI = require('../utils/tryDecodeURI')
+const readFileAsync = promisify(fs.readFile);
+const tryDecodeURI = require('../utils/tryDecodeURI');
 
 function historySearcher(query, profileFolderPath, pluginContext) {
   const historyDbPath = path.join(profileFolderPath, 'places.sqlite');
   pluginContext.console.log('warn', 'search history db', {
     historyDbPath,
   });
-  return sqlite.open(historyDbPath).then(historyDb => {
-    return historyDb
-      .all(
+  return initSqlJs().then(SQL =>
+    readFileAsync(historyDbPath).then(dbFile => {
+      const historyDb = new SQL.Database(dbFile);
+
+      const result = historyDb.exec(
         `
-      SELECT last_visit_date, frecency, url, title FROM 'moz_places'
-      WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
-      ORDER BY frecency DESC, last_visit_date DESC;
-    `,
-      )
-      .then(resultList =>
-        resultList.map(({ title, url, last_visit_date: date, frecency }) => ({
-          title,
-          subtitle: tryDecodeURI(url),
-          value: url,
-          icon: 'fa-history',
-          id: `${date}-${frecency}`,
-        })),
+          SELECT title, url, last_visit_date, frecency FROM 'moz_places'
+          WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
+          ORDER BY frecency DESC, last_visit_date DESC;
+        `,
       );
-  });
+      const resultList = result[0].values;
+      return resultList.map(([title, url, date, frecency]) => ({
+        title,
+        subtitle: tryDecodeURI(url),
+        value: url,
+        icon: 'fa-history',
+        id: `${date}-${frecency}`,
+      }));
+    }),
+  );
 }
 
 module.exports = historySearcher;
