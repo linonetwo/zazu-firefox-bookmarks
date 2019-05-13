4ad0ee6913098abc7ed82b3d9444573ba52e0c1f
linonetwo <linonetwo012@gmail.com>
2019-05-13 14:14:45 +0800

    Revert "refactor: try trilogy"
    This reverts commit abcbf91b9aab68c6294c5df703af0d8d59e31f98.

--- src/history/historySearcher.js
+++ src/history/historySearcher.js
@@ -1,30 +1,24 @@
 // @ts-check
-const Trilogy = require('trilogy');
+const sqlite = require('sqlite');
 const path = require('path');
 
-const tryDecodeURI = require('../utils/tryDecodeURI');
+const tryDecodeURI = require('../utils/tryDecodeURI')
 
 function historySearcher(query, profileFolderPath, pluginContext) {
   const historyDbPath = path.join(profileFolderPath, 'places.sqlite');
   pluginContext.console.log('warn', 'search history db', {
     historyDbPath,
   });
-  // console.log(Trilogy);
-
-  const db = new Trilogy(historyDbPath, {
-    client: 'sql.js',
-  });
-  return db
-    .raw(
-      db.knex.raw(`
-        SELECT last_visit_date, frecency, url, title FROM 'moz_places'
-        WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
-        ORDER BY frecency DESC, last_visit_date DESC;
-      `),
-      true,
-    )
-    .then(resultList =>
-      db.close().then(() =>
+  return sqlite.open(historyDbPath).then(historyDb => {
+    return historyDb
+      .all(
+        `
+      SELECT last_visit_date, frecency, url, title FROM 'moz_places'
+      WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
+      ORDER BY frecency DESC, last_visit_date DESC;
+    `,
+      )
+      .then(resultList =>
         resultList.map(({ title, url, last_visit_date: date, frecency }) => ({
           title,
           subtitle: tryDecodeURI(url),
@@ -32,8 +26,8 @@ function historySearcher(query, profileFolderPath, pluginContext) {
           icon: 'fa-history',
           id: `${date}-${frecency}`,
         })),
-      ),
-    );
+      );
+  });
 }
 
 module.exports = historySearcher;
