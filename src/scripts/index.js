d4b9fba166d08fc36de8ea22264496a6304fb811
linonetwo <linonetwo012@gmail.com>
2019-05-15 20:45:31 +0800

    fix: curied function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,13 @@
 const firefoxHistoryBookmarkSearch = require('./searchAll');
 
-function search() {
+function search(pluginContext) {
+  const searcher = firefoxHistoryBookmarkSearch(pluginContext)
   return {
     respondsTo(query, env = {}) {
       return env.prefix === false && query.length > 1;
     },
     search(query, env) {
-      return firefoxHistoryBookmarkSearch(query, env);
+      return searcher(query, env);
     },
   };
 }
