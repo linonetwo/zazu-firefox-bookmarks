12b7503f5d0e43ebb3acff4677b246114d6ced41
linonetwo <linonetwo012@gmail.com>
2019-05-15 12:59:45 +0800

    Revert "feat: try sqlite3"
    This reverts commit e9968969bb35b597c234fc079230115aa7035a61.

--- src/history/historySearcher.js
+++ src/history/historySearcher.js
@@ -1,46 +1,40 @@
 // @ts-check
-const sqlite3 = require('sqlite3');
+const initSqlJs = require('sql.js');
 const path = require('path');
 const promisify = require('util.promisify');
 const fs = require('fs');
-const _ = require('lodash');
+const _ = require('lodash')
 
-const copyFileAsync = promisify(fs.copyFile);
+const readFileAsync = promisify(fs.readFile);
 const tryDecodeURI = require('../utils/tryDecodeURI');
 
-async function historySearcher(query, profileFolderPath, limit, pluginContext) {
+function historySearcher(query, profileFolderPath, limit, pluginContext) {
   const historyDbPath = path.join(profileFolderPath, 'places.sqlite');
   pluginContext.console.log('info', 'search history db', {
     historyDbPath,
   });
-  const tmpHistoryDbPath = `${historyDbPath}.tmp`;
-  try {
-    await copyFileAsync(historyDbPath, tmpHistoryDbPath);
-    const historyDb = new sqlite3.Database(tmpHistoryDbPath, sqlite3.OPEN_READONLY);
-    const all = sql =>
-      new Promise((resolve, reject) =>
-        historyDb.all(sql, (error, result) => (error ? reject(error) : resolve(result))),
+  return initSqlJs().then(SQL =>
+    readFileAsync(historyDbPath).then(dbFile => {
+      const historyDb = new SQL.Database(dbFile);
+
+      const result = historyDb.exec(
+        `
+          SELECT title, url, last_visit_date, frecency FROM 'moz_places'
+          WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
+          ORDER BY frecency DESC, last_visit_date DESC
+          LIMIT ${limit};
+        `,
       );
-    const resultList = await all(`
-    SELECT title, url, last_visit_date, frecency FROM 'moz_places'
-    WHERE url LIKE '%${query}%' OR title LIKE '%${query}%'
-    ORDER BY frecency DESC, last_visit_date DESC
-    LIMIT ${limit};
-    `);
-    return resultList.map(({ title, url, last_visit_date: date, frecency }) => ({
-      title,
-      subtitle: tryDecodeURI(url),
-      value: url,
-      icon: 'fa-history',
-      id: `${date}-${frecency}`,
-    }));
-  } catch (error) {
-    pluginContext.console.error('search history db failed', {
-      error,
-      errorString: String(error),
-    });
-    return [];
-  }
+      const resultList = _.get(result, '[0].values', []);
+      return resultList.map(([title, url, date, frecency]) => ({
+        title,
+        subtitle: tryDecodeURI(url),
+        value: url,
+        icon: 'fa-history',
+        id: `${date}-${frecency}`,
+      }));
+    }),
+  );
 }
 
 module.exports = historySearcher;
